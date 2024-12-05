f952dd8b0b331b76a67e6e07672972b5be84a3c1
Alexpiloso <alexpiloso332004@gmail.com>
2024-12-05 11:39:50 -0500

    Examen

---
+++ src/main/java/ec/edu/espoch/alexloor2025/Calculadora.java
@@ -0,0 +1,28 @@
+package ec.edu.espoch.alexloor2025;
+
+public class Calculadora {
+
+    public int num1;
+    public int num2;
+
+    public Calculadora(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+}
